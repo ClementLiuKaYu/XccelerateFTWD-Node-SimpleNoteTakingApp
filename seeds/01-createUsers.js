0bd413416d04827f3b9162992e4f1cd8ec691404
ClementLiuKaYu <76940127+ClementLiuKaYu@users.noreply.github.com>
2021-07-20 15:38:50 +0800

    knex update
    update seed,
    update knexfile

--- seeds/01-createUsers.js
+++ seeds/01-createUsers.js
@@ -1,18 +1,5 @@
-const bcrypt = require("bcrypt");
-let hashedPassword = (password) => {
-  return new Promise((resolve, reject) => {
-    bcrypt.hash(password, 10, (err, hash) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(hash);
-    });
-  });
-};
-
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  const password = await hashedPassword("1234");
   return knex("users")
     .del()
     .then(function () {
